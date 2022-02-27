5d83dc01dafdb18e4c3454dda13ee043da571e42
gatewayitconsultingDesktop <43389272+gatewayitconsulting@users.noreply.github.com>
2022-02-27 03:11:25 -0600

    Update Montanye Wedding project

--- src/pages/projectsPage/sections/montanyeWedding.js
+++ src/pages/projectsPage/sections/montanyeWedding.js
@@ -10,7 +10,7 @@ import Link from '@mui/material/Link';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import { Typography } from "@mui/material";
-import { containerFluid, main, my1 } from "../../../assets/styles/neomat";
+import { containerFluid, main, my1, my2, my3, textCenter } from "../../../assets/styles/neomat";
 
 // icons
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
@@ -18,7 +18,8 @@ import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 // images
 import montanyeWeddingHomePage from "../../../assets/imgs/projects/montanye-wedding/montanye-wedding-home.png";
 import montanyeWeddingWhenWherePage from "../../../assets/imgs/projects/montanye-wedding/montanye-wedding-whenwhere.png";
-import montanyeWeddingRSVPPage from "../../../assets/imgs/projects/montanye-wedding/montanye-wedding-rsvp.png";
+import montanyeWeddingRSVPPage from "../../../assets/imgs/projects/montanye-wedding/montanye-wedding-vendors.png";
+import bottomImage from "../../../assets/imgs/projects/montanye-wedding/montanye-wedding-rsvp.png";
 
 const backgroundColor = css`
     background-color: #222;
@@ -32,16 +33,16 @@ const marginTop = css`
     padding-top: 5em;
 `
 
-const displayBlock = css`
-    display: block;
-    margin-bottom: 2em;
-`
-
 const cursiveFont = css`
     font-family: "Alex Brush", cursive;
     font-weight: 500;
     line-height: 1.5em;
     font-size: 1.5rem;
+`
+
+const bottomPortfolioImage = css`
+    margin-top: 1em;
+    width: 100%;
 `
 
 export default function MontanyeWedding() {
@@ -79,34 +80,23 @@ export default function MontanyeWedding() {
                     <Typography variant="h3" gutterBottom component="div">
                         Montanye Wedding
                     </Typography>
-                    <div className={cx(displayBlock)}>
-                        <Typography variant="h5" component="span" sx={{ marginRight: '.5em' }}>
-                            The Challenge:
-                        </Typography>
-                        <Typography variant="body1" component="span">
-                            The Montanyes were looking to get married and wanted a premium and faithfully artistic representation of their relationship to convey to those looking for a glimpse of their big day. Fortunately the groom-to-be, John Montanye (the designer/developer of JMDesigns.io) was happy to take on the project and pour his heart/soul into making a website that did just that.
-                        </Typography>
-                    </div>
-                    <div className={cx(displayBlock)}>
-                        <Typography variant="h5" component="span" sx={{ marginRight: '.5em' }}>
-                            The Process:
-                        </Typography>
-                        <Typography variant="body1" component="span">
-                            John knew that he could come up with the code to accomplish whatever bride-to-be Lauren wanted done, so the couple began doing design discoveries to come up with an overall look and feel to the website. Lauren and John's taste was rather dark but they wanted a refined approach. They picked out basic structure and settled on the beautiful cursive font:
-                            <br />
-                            <span className={cx(cursiveFont)}>Alex Brush</span>
-                            <br />
-                            After the initial design was picked out, John set out to find a framework and UI library that could help him develop the website and also save time (he started working at New Balance when this project took off). He came up with a Creative Tim developed React and Material Design theme. Building off the base theme and modifying the existing theme significantly, John was able to tailor the theme to his and Lauren's exact creative needs. By adding extras like being able to Save the Date to calendars, an animated timeline that highlighted favorite moments from the relationship, a countdown timer to the event date, a vendors page to show off vendors from the event,
-                        </Typography>
-                    </div>
-                    <div className={cx(displayBlock)}>
-                        <Typography variant="h5" component="span" sx={{ marginRight: '.5em' }}>
-                            The Result:
-                        </Typography>
-                        <Typography variant="body1" component="span">
-                            Lauren and John were able to create a very customized website built off a very feature-rich theme base.
-                        </Typography>
-                    </div>
+                    <Typography variant="body1" gutterBottom component="div">
+                        The Montanyes were looking to get married and wanted a premium and faithfully artistic representation of their relationship to convey to those looking for a glimpse of their big day. Fortunately, the groom-to-be, John Montanye (the designer/developer of JMDesigns.io) was happy to take on the project and pour his heart/soul into making a website that did just that.
+                    </Typography>
+                    <Typography variant="body1" gutterBottom component="div">
+                        John knew that he could come up with the code to accomplish whatever bride-to-be Lauren wanted done, so the couple began doing design discoveries to come up with an overall look and feel to the website. Lauren and John's taste was rather dark, but they wanted a refined approach. They picked out basic structure and settled on the beautiful cursive font: <span className={cx(cursiveFont)}>Alex Brush</span>
+                    </Typography>
+                    <Typography variant="body1" gutterBottom component="div">
+                        After the initial design was picked out, John set out to find a framework and UI library that could help him develop the website and also save time (he started working at New Balance when this project took off). He came up with a Creative Tim developed React and Material Design theme. Building off the base theme and modifying the existing theme significantly, John was able to tailor the theme to his and Lauren's exact creative needs. By adding extras like being able to Save the Date to calendars, an animated timeline that highlighted favorite moments from the relationship, a countdown timer to the event date, a vendors page to show off vendors from the event, and an RSVP page.
+                    </Typography>
+                    <Typography variant="body1" gutterBottom component="div">
+                        Lauren and John were able to create a very customized website built off a very feature-rich theme base.
+                    </Typography>
+                </div>
+                <div className={cx(textCenter, my3)}>
+                    <Typography variant="h4" component="span">
+                        Gallery
+                    </Typography>
                 </div>
                 <ImageList sx={{ width: '100%' }} variant="woven" cols={3} gap={12}>
                     {itemData.map((item) => (
@@ -120,8 +110,14 @@ export default function MontanyeWedding() {
                         </ImageListItem>
                     ))}
                 </ImageList>
+                <div>
+                    <img
+                        src={bottomImage}
+                        className={cx(bottomPortfolioImage)}
+                    />
+                </div>
                 <Footer />
             </div>
-        </div>
+        </div >
     );
 }
