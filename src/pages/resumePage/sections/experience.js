4af5aa25637eb2241121e81ed2b064ee6654745c
John Montanye <43389272+gatewayitconsulting@users.noreply.github.com>
2022-07-13 23:50:22 -0500

    Update experience
    Changed experience page copy

--- src/pages/resumePage/sections/experience.js
+++ src/pages/resumePage/sections/experience.js
@@ -11,6 +11,7 @@ import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 import SubjectIcon from '@mui/icons-material/Subject';
 
 // images
+import ignitionCommerceLogo from "../../../assets/imgs/ignition-commerce-logo.png";
 import newbalanceLogo from "../../../assets/imgs/newbalance.svg";
 import syllogisteksLogo from "../../../assets/imgs/syllogisteks.ico";
 import gatewayitconsultingLogo from "../../../assets/imgs/gitc.svg";
@@ -28,14 +29,88 @@ export default function ExperienceSection() {
                 <Grid item xs={12}>
                     <div style={{ display: "flex", alignItems: "center", borderTop: "1px solid #FFF", paddingTop: "1em" }}>
                         <RadioButtonCheckedIcon style={{ marginRight: ".25em", boxShadow: "0px 13px 27px -5px #000, 0px 8px 16px -8px #222" }} />
-                        <Typography variant="body1">March, 2021 - Current</Typography>
+                        <Typography variant="body1">May, 2022 - Current</Typography>
+                    </div>
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                        <img src={ignitionCommerceLogo} style={{ width: "20px", marginRight: ".5em" }} alt="Ignition Commerce logo" />
+                        <Typography variant="h4" component="h3" style={{ display: "inline" }}>Ignition Commerce</Typography>
+                    </div>
+                    <Typography variant="h5" component="h3" style={{ display: "inline" }}>Salesforce Commerce Cloud Developer</Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em", marginTop: ".25em" }}>- Currently working</Typography>
+                </Grid>
+                <Grid item xs={12} style={{ marginTop: "3em" }}>
+                    <div style={{ display: "flex", alignItems: "center", borderTop: "1px solid #FFF", paddingTop: "1em" }}>
+                        <RadioButtonUncheckedIcon style={{ marginRight: ".25em", boxShadow: "0px 13px 27px -5px #000, 0px 8px 16px -8px #222" }} />
+                        <Typography variant="body1">March, 2020 - May, 2022</Typography>
                     </div>
                     <div style={{ display: "flex", alignItems: "center" }}>
                         <img src={newbalanceLogo} style={{ width: "50px", marginRight: ".5em" }} alt="New Balance logo" />
                         <Typography variant="h4" component="h3" style={{ display: "inline" }}>New Balance</Typography>
                     </div>
                     <Typography variant="h5" component="h3" style={{ display: "inline" }}>Software Engineer</Typography>
-                    <Typography variant="body1" style={{ marginLeft: "1em", marginTop: ".25em" }}>- Currently working</Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Developed and integrated the Emarsys email automation programs and emails for the Malaysia
+                        website release using Emarsys, Deck Commerce, HTML, ESL (Emarsys Scripting Language), SFRA, and
+                        SFCC.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Participated in several coding and international business activities to migrate Magento website to
+                        SFRA for flagship release of Hong Kong website.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Developed several external facing website sections using HTML5, Bootstrap, CSS3, JavaScript, and
+                        Salesforce Commerce Cloud for flagship transitioning releases of IT, PT, ES, BE, AT, NL, DE, FR, UK,
+                        Malaysia, and Taiwan websites to SFRA platform.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Developed several Salesforce Commerce Cloud components for internal facing business user CMS
+                        (Page Designer) to automate work processes using ISML, HTML, SASS, and JavaScript.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Developed a video enhancement for various Salesforce Page Designer components and for all other
+                        videos on NB sites to auto play inline on user scroll and be ADA compliant using HTML5, Bootstrap,
+                        CSS3, and JavaScript.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Created a custom attribute editor to expand the functionality of Salesforce Page Designer using
+                        ISML, HTML, SASS, and JavaScript.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Developed internal facing interactive style guide using HTML, SASS, React, and JavaScript.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Wrote unit tests for external facing features using JavaScript, Mocha, and Sinon.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Connected and tested emails for various releases using Emarsys platform and JavaScript.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Worked with SAFe agile practices including sprints, project planning and innovation sprints,
+                        retrospectives, train synchs, and refinement sessions. Used sprint boards to communicate workflow
+                        among primary team. Participated in daily standups with stakeholders, product owners, project
+                        managers, designers, and other developers, often internationally to communicate workflows.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Conducted and wrote several reports that demonstrated and measured the impact of Page Designer
+                        on automation of developer processes.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Wrote technical and organizational documentation for developers and business users.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Handled several Salesforce access requests and managed users’ access and permissions to multiple
+                        environments, at times creating or modifying roles and permissions between regions and
+                        environments.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Worked with multiple projects and complex daily timelines to produce deliverables on time.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Used version control system Git through SourceTree and CLI to collaborate on projects.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Code reviewed team members code daily.
+                    </Typography>
                 </Grid>
                 <Grid item xs={12} style={{ marginTop: "3em" }}>
                     <div style={{ display: "flex", alignItems: "center", borderTop: "1px solid #FFF", paddingTop: "1em" }}>
@@ -54,21 +129,54 @@ export default function ExperienceSection() {
                         <img src={newbalanceLogo} style={{ width: "25px", marginRight: ".5em" }} alt="New Balance small logo" />
                         <Typography variant="h5" component="h3" style={{ display: "inline" }}>UI Developer at New Balance</Typography>
                     </div>
-                    <Typography variant="body1" style={{ marginTop: ".5em", marginBottom: ".5em" }}>
-                        <i>Technologies used replaced with "various technologies" to maintain client privacy.</i>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Developed several external facing website sections using HTML5, Bootstrap, CSS3, JavaScript, and
+                        Salesforce Commerce Cloud for flagship transitioning releases of US, CA, AU, NZ, BE, AT, NL, DE, FR,
+                        and UK websites.
                     </Typography>
-                    <Typography variant="body1" style={{ marginLeft: "1em" }}>- Developed several external facing website sections using various technologies for flagship transitioning releases of US, CA, AU, NZ, BE, AT, NL, DE, FR, and UK websites.</Typography>
-                    <Typography variant="body1" style={{ marginLeft: "1em" }}>- Developed Salesforce Commerce Cloud component for internal facing business user CMS to automate work processes using various technologies.</Typography>
-                    <Typography variant="body1" style={{ marginLeft: "1em" }}>- Translated complex, multi-layer Photoshop and inVision creatives into pixel perfect, responsive, ADA compliant external facing web pages, daily, for US and CA websites using various technologies.</Typography>
-                    <Typography variant="body1" style={{ marginLeft: "1em" }}>- Developed and ensured that code was ADA compliant and translated properly, to match French language laws, from US external facing website to CA external facing website, adjusting designs when necessary to retain design integrity.</Typography>
-                    <Typography variant="body1" style={{ marginLeft: "1em" }}>- Used Photoshop to export optimized web safe images from complex and multi-layered creatives.</Typography>
-                    <Typography variant="body1" style={{ marginLeft: "1em" }}>- Trained team members how to use various systems and how to write accurate and DRY code that upheld ADA compliance.</Typography>
-                    <Typography variant="body1" style={{ marginLeft: "1em" }}>- Code reviewed team members code daily.</Typography>
-                    <Typography variant="body1" style={{ marginLeft: "1em" }}>- Held cross team meetings to demo and present web page projects with product owners and designers, at times Internationally.</Typography>
-                    <Typography variant="body1" style={{ marginLeft: "1em" }}>- Wrote organizational documentation for systems as discovered through development and usage.</Typography>
-                    <Typography variant="body1" style={{ marginLeft: "1em" }}>- Agile experience – bi-weekly sprints, weekly grooming sessions, used Kanban/sprint boards to communicate workflow among primary team and other teams related to projects. Participated in daily standups with stakeholders, product owners, project managers, designers, and other developers, often Internationally to communicate workflows.</Typography>
-                    <Typography variant="body1" style={{ marginLeft: "1em" }}>- Worked with multiple projects and complex daily timelines to produce deliverables on time.</Typography>
-                    <Typography variant="body1" style={{ marginLeft: "1em" }}>- Used version control system to collaborate on projects.</Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Developed Salesforce Commerce Cloud component for internal facing business user CMS to
+                        automate work processes using ISML, HTML, SASS, and JavaScript.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Translated complex, multi-layer Photoshop and inVision creatives into pixel perfect, responsive, ADA
+                        compliant external facing web pages, daily, for US and CA websites using HTML5, Bootstrap,
+                        Foundations, CSS3, and JavaScript.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Developed and ensured that code was ADA compliant and translated properly, to match French
+                        language laws, from US external facing website to CA external facing website, adjusting designs
+                        when necessary to retain design integrity.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Used Photoshop to export optimized web safe images from complex and multi-layered creatives.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Trained team members how to use various systems and how to write accurate and DRY code that
+                        upheld ADA compliance.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Code reviewed team members code daily.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Held cross team meetings to demo and present web page projects with product owners and
+                        designers, at times Internationally.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Wrote organizational documentation for systems as discovered through development and usage.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Agile experience – bi-weekly sprints, weekly grooming sessions, used Kanban/sprint boards to
+                        communicate workflow among primary team and other teams related to projects. Participated in
+                        daily standups with stakeholders, product owners, project managers, designers, and other
+                        developers, often Internationally to communicate workflows.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Worked with multiple projects and complex daily timelines to produce deliverables on time.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Used version control system Git through SourceTree and CLI to collaborate on projects.
+                    </Typography>
                     <div style={{ display: "flex", alignItems: "center", marginTop: "1em" }}>
                         <SubjectIcon style={{ marginRight: ".25em", boxShadow: "0px 13px 27px -5px #000, 0px 8px 16px -8px #222" }} />
                         <Typography variant="body1">March, 2018 - March, 2020</Typography>
@@ -101,6 +209,15 @@ export default function ExperienceSection() {
                         <Typography variant="h4" component="h3" style={{ display: "inline" }}>Gateway IT Consulting</Typography>
                     </div>
                     <Typography variant="body1" style={{ marginTop: ".25em", marginBottom: ".25em" }}><i>Clients include internal and Our Lady's Inn.</i></Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Designed and developed external facing digital portfolio website using React, Material Design (MUI),
+                        JavaScript, HTML5, CSS.
+                    </Typography>
+                    <Typography variant="body1" style={{ marginLeft: "1em" }}>
+                        - Designed and developed external facing wedding website that interfaced with API for GET/POST
+                        requests using React, Material Design, JavaScript, HTML5, CSS, PostgreSQL, Nodejs, Google Analytics,
+                        and Mailchimp.
+                    </Typography>
                     <Typography variant="body1" style={{ marginLeft: "1em" }}>- Project managed from a technical perspective, successful virtual marathon for response to COVID business impact.</Typography>
                     <Typography variant="body1" style={{ marginLeft: "1em" }}>- Prototyped in inVision and developed TypeScript, Angular 11, Material Design, SASS, HTML5, external facing, data driven dashboard.</Typography>
                     <Typography variant="body1" style={{ marginLeft: "1em" }}>- Designed and developed JavaScript, Angular 7, Bootstrap 4, SASS, HTML5, JSON-using external facing, responsive application.</Typography>
